a277f8afb6265fd4637a228766ab772e51f7b9bd
Pavan Bhat <pavanagasal007@gmail.com>
2024-02-29 21:20:51 +0530

    first commit

---
+++ src/test/java/Project1/OrangeHRM/Login.java
@@ -0,0 +1,46 @@
+package Project1.OrangeHRM;
+
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import org.testng.annotations.Test;
+
+public class Login {
+
+	public static WebDriver driver;
+	@Test
+	public void log(){
+	
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\ADMIN\\Documents\\driver\\chromedriver-win64\\chromedriver.exe");
+
+	driver = new ChromeDriver();
+	driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
+	
+	WebElement username = driver.findElement(By.xpath("//input[@name='username']"));
+	username.click();
+	username.sendKeys("Admin");
+	
+	WebElement password = driver.findElement(By.xpath("//input[@name='password']"));
+	password.click();
+	password.sendKeys("admin123");
+	
+	WebElement submit = driver.findElement(By.xpath("//button[normalize-space()='Login']"));
+	submit.click();
+	System.out.println("Login Successful");
+	
+	
+       
+        
+    }
+  
+    
+    }
